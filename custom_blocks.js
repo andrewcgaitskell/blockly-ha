cf8839e88ae3a12d0d5a0cff6149284fad02385b
andrewcgaitskell <andrew@gaitskell.com>
2026-09-23 18:12:25 +0100

    Update custom_blocks.js

--- custom_blocks.js
+++ custom_blocks.js
@@ -1,31 +1,18 @@
-// This function triggers automatically the exact millisecond Webpack loads
-window.setupMyBlocks = function(Blockly) {
-  
-  // 1. Create your block layout
-  Blockly.Blocks['my_simple_block'] = {
-    init: function() {
-      this.appendDummyInput().appendField("Simple Drag & Drop Block");
-      this.setPreviousStatement(true, null);
-      this.setNextStatement(true, null);
-      this.setColour(120);
-    }
-  };
+// Register the block object interface into the shared global namespace
+if (typeof Blockly === 'undefined') {
+    var Blockly = { Blocks: {}, JavaScript: {} };
+}
 
-  // 2. Wait just a moment for the visual workspace to draw, then add it to the menu
-  setTimeout(() => {
-    const workspace = Blockly.getMainWorkspace();
-    const toolbox = workspace.getOptions().languageTree;
-    
-    if (toolbox && toolbox.contents) {
-      toolbox.contents.push({
-        "kind": "category",
-        "name": "Custom Blocks",
-        "contents": [
-          { "kind": "block", "type": "my_simple_block" }
-        ]
-      });
-      workspace.updateToolbox(toolbox);
-    }
-  }, 100);
+Blockly.Blocks['my_simple_block'] = {
+  init: function() {
+    this.appendDummyInput().appendField("Simple Drag & Drop Block");
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(120);
+  }
+};
+
+Blockly.JavaScript['my_simple_block'] = function(block) {
+  return "// Simple block dropped\n";
 };
 
