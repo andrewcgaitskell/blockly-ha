c3e396d8b0e703b10adb94018071b62ee40f37e8
andrewcgaitskell <andrew@gaitskell.com>
2026-09-23 18:25:21 +0100

    clean up

--- blocks/text.d.ts
+++
@@ -1,9 +0,0 @@
-/**
- * @license
- * Copyright 2023 Google LLC
- * SPDX-License-Identifier: Apache-2.0
- */
-export declare const blocks: {
-    [key: string]: any;
-};
-//# sourceMappingURL=text.d.ts.map
