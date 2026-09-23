424313c5fe334f0cb92fd0422337602ed3704ee3
andrewcgaitskell <andrew@gaitskell.com>
2026-09-23 18:19:44 +0100

    Update custom_blocks.js

--- custom_blocks.js
+++ custom_blocks.js
@@ -1,18 +1,16 @@
-// Register the block object interface into the shared global namespace
-if (typeof Blockly === 'undefined') {
-    var Blockly = { Blocks: {}, JavaScript: {} };
-}
-
-Blockly.Blocks['my_simple_block'] = {
-  init: function() {
-    this.appendDummyInput().appendField("Simple Drag & Drop Block");
-    this.setPreviousStatement(true, null);
-    this.setNextStatement(true, null);
-    this.setColour(120);
+// Define the visual layout of your block
+Blockly.common.defineBlocksWithJSONArray([
+  {
+    "type": "my_simple_block",
+    "message0": "Simple Drag & Drop Block",
+    "previousStatement": null,
+    "nextStatement": null,
+    "colour": 120
   }
-};
+]);
 
+// Define the output generator logic
 Blockly.JavaScript['my_simple_block'] = function(block) {
-  return "// Simple block dropped\n";
+  return "console.log('Simple block executed!');\n";
 };
 
